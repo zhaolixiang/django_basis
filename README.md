b2a5509f49f1a1871f0a19fddd8e1a5e8dcb7e70
mark <1782980833@qq.com>
2020-02-19 00:02:18 +0800

    finish

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # django_basis
 Django 基础 http://www.handsomemark.com/
+
+# 数据初始化或者迁移：
+```
+python manage.py makemigrations rango
+python manage.py migrate
+python manage.py createsuperuser
+3580445281ww
+
+# 启动测试
+python manage.py runserver
+
+http://127.0.0.1:8000/admin/login/?next=/admin/
+```
--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -13,15 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf import settings
 from django.conf.urls import url, include
-from django.conf.urls.static import static
 from django.contrib import admin
-
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
-    url(r'^rango/',include('rango.urls')),
+    url(r'^$', views.index, name='index'),
+    url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    url(r'^accounts/register/$', views.RangoRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
